892f457f2b56c2e572e3f2daf74f4b86bc5a4a28
Sumitkumar Shingavi <45287362+sumitshingavi@users.noreply.github.com>
2018-12-10 12:14:34 +0800

    Third

---
+++ scripts/Deploy.cmd
@@ -0,0 +1,8 @@
+
+#Commands for GIT commits
+git init
+git add README.md
+git commit -m "FirstCommit"
+git remote add origin https://github.com/sumitshingavi/sfdxunlockpackagestructure.git
+git push -u origin master
+git status
